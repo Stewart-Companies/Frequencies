8ea7bcf075ab654b0ee7c37ca9e91d9c5e9c66f4
itscory <corey@Coreys-Air.fios-router.home>
2015-12-09 11:21:23 -0500

    brand new Xcode project

---
+++ Sources/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  FrequenciesPro
+//
+//  Created by Corey Stewart on 12/9/15.
+//  Copyright © 2015 Stewart Companies. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
